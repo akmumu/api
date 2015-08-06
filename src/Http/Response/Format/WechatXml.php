9720f413195778899e076299f2fd5a26ccc9345b
akmumu <297193792@qq.com>
2015-08-06 18:24:36 +0800

    getWechatXml and parse

--- src/Http/Response/Format/WechatXml.php
+++ src/Http/Response/Format/WechatXml.php
@@ -6,6 +6,9 @@ use Illuminate\Contracts\Support\Arrayable;
 
 class WechatXml extends Format
 {
+    public $request = array();    
+    protected $funcflag = false;
+
     /**
      * Format an Eloquent model.
      *
@@ -61,8 +64,65 @@ class WechatXml extends Format
         array_walk_recursive($content, function (&$value) {
             $value = $this->morphToArray($value);
         });
+        $this->getWechatXml($content['detail']);
+        // print_r($this->request);
+        if ($content['type'] == 'text')
+        {
+            return $this->replyText($content='wocao');
+        }
 
-        return $this->encode($content);
+        return $this->encode([]);
+    }
+    
+    //获取微信发来带有用户消息的XML
+    public function getWechatXml($xml='')
+    {
+        $postObj = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
+        $request = (array)$postObj;
+
+        // 可以解析信息的详细信息
+        // $fromUsername = $postObj->FromUserName;
+        // $toUsername = $postObj->ToUserName;
+        // $Message = trim($postObj->Content);
+        // $MsgType = trim($postObj->MsgType);
+        
+        // $Location_X = $postObj->Location_X;
+        // $Location_Y = $postObj->Location_Y;
+        // $Scale = $postObj->Scale;
+        // $Label = $postObj->Label;
+        // $PicUrl = $postObj->PicUrl;
+        // $MsgId  = $postObj->MsgId;
+        // $Url = $postObj->Url;
+        // $Event = $postObj->Event;
+        // $Latitude = $postObj->Latitude;
+        // $Longitude = $postObj->Longitude;
+        // $Precision = $postObj->Precision;
+        // $EventKey = $postObj->EventKey;
+
+        $this->request = $request;
+    }
+
+    //星标消息
+    public function set_funcflag()
+    {
+        $this->funcflag = true;
+    }
+
+    //回复文本
+    public function replyText($message)
+    {
+        $textTpl = <<<eot
+<xml>
+    <ToUserName><![CDATA[%s]]></ToUserName>
+    <FromUserName><![CDATA[%s]]></FromUserName>
+    <CreateTime>%s</CreateTime>
+    <MsgType><![CDATA[%s]]></MsgType>
+    <Content><![CDATA[%s]]></Content>
+    <FuncFlag>%d</FuncFlag>
+</xml>
+eot;
+        $req = $this->request;
+        return sprintf($textTpl, $req['FromUserName'], $req['ToUserName'], time(), 'text', $message, $this->funcflag ? 1 : 0);
     }
 
     /**
